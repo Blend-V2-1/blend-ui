011d4c1b7b7cf2aeac1cacbc74289f267b4f1036
Donovan Levinson <skipatrol@gmail.com>
2026-09-15 08:23:49 -1000

    Remove testnet asset faucet banner

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,8 +1,6 @@
 import { Box } from '@mui/material';
-import { Networks } from '@stellar/stellar-sdk';
 import { useRouter } from 'next/router';
 import { ReactNode, useEffect } from 'react';
-import { FaucetBanner } from '../components/common/FaucetBanner';
 import { OverlayModal } from '../components/common/OverlayModal';
 import { OverlayModalTOS } from '../components/common/OverlayModalTOS';
 import { Row } from '../components/common/Row';
@@ -10,7 +8,7 @@ import { WalletWarning } from '../components/common/WalletWarning';
 import { NavBar } from '../components/nav/NavBar';
 import { useSettings, ViewType } from '../contexts';
 import { useWallet } from '../contexts/wallet';
-import { useBackstop, useFeeStats, usePoolMeta } from '../hooks/api';
+import { useFeeStats, usePoolMeta } from '../hooks/api';
 
 export default function DefaultLayout({ children }: { children: ReactNode }) {
   const { viewType, trackPool, setLastPool } = useSettings();
@@ -19,17 +17,9 @@ export default function DefaultLayout({ children }: { children: ReactNode }) {
   const { poolId } = router.query;
   const safePoolId =
     typeof poolId == 'string' && /^[0-9A-Z]{56}$/.test(poolId) ? poolId : undefined;
-  const isTestnet = process.env.NEXT_PUBLIC_PASSPHRASE === Networks.TESTNET;
 
   const { data: poolMeta } = usePoolMeta(safePoolId as string, safePoolId !== undefined);
-  const { data: backstop } = useBackstop(poolMeta?.deployment);
   const { data: feeStats } = useFeeStats();
-
-  // get the last (oldest) pool in the reward zone
-  const faucet_pool =
-    backstop !== undefined && backstop.config.rewardZone.length > 0
-      ? backstop.config.rewardZone[backstop.config.rewardZone.length - 1]
-      : undefined;
 
   useEffect(() => {
     if (poolMeta !== undefined) {
@@ -73,11 +63,6 @@ export default function DefaultLayout({ children }: { children: ReactNode }) {
             <Row>
               <WalletWarning />
             </Row>
-            {faucet_pool !== undefined && isTestnet && (
-              <Row>
-                <FaucetBanner poolId={faucet_pool} />
-              </Row>
-            )}
             {children}
             <OverlayModal />
             <OverlayModalTOS />
